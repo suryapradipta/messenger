dae9cae7b41392b92d51fef9a28fe31873cc16e3
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2023-08-15 11:03:24 +0800

    navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'destinations.dart';
 import 'models/data.dart' as data;
 import 'models/models.dart';
 import 'widgets/email_list_view.dart';
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.light(useMaterial3: true),
       home: Feed(currentUser: data.user_0),
     );
@@ -37,12 +39,22 @@ class _FeedState extends State<Feed> {
   late final _backgroundColor = Color.alphaBlend(
       _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
 
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         color: _backgroundColor,
         child: EmailListView(
+
+          selectedIndex: selectedIndex,
+          onSelected: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+
           currentUser: widget.currentUser,
         ),
       ),
@@ -51,6 +63,23 @@ class _FeedState extends State<Feed> {
         foregroundColor: _colorScheme.onTertiaryContainer,
         onPressed: () {},
         child: const Icon(Icons.add),
+      ),
+
+      bottomNavigationBar: NavigationBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        destinations: destinations.map<NavigationDestination>((d) {
+          return NavigationDestination(
+            icon: Icon(d.icon),
+            label: d.label,
+          );
+        }).toList(),
+        selectedIndex: selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
       ),
     );
   }
